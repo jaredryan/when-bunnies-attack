c5da975a850fb9b92cb74ad309d8ceb51a2a10ab
Jared Ryan <jared@mantl.com>
2025-10-01 13:34:16 -0700

    further cleanup for if I wanted to have fun with colors and background images

--- src/Components/Modal.js
+++ src/Components/Modal.js
@@ -22,34 +22,37 @@ export const Modal = ({
   }, [open]);
 
   return (
-    <dialog
-      ref={dialogRef}
-      onCancel={onClose}
-      className={`dialog ${open ? "open" : "closed"}${className ? ` ${className}` : ""}`}
-      style={style}
-    >
-      {noClose ? null : (
-        <button className="xButton" onClick={onClose}>
-          <svg
-            aria-hidden="true"
-            width="16"
-            height="16"
-            viewBox="0 0 24 24"
-            fill="none"
-            xmlns="http://www.w3.org/2000/svg"
-          >
-            <path
-              d="M18 6L6 18M6 6L18 18"
-              stroke="currentColor"
-              strokeWidth="2"
-              strokeLinecap="round"
-              strokeLinejoin="round"
-            />
-          </svg>
-        </button>
-      )}
-      {children}
-    </dialog>
+    <>
+      <div className={`modalBackgroundOverlay${open ? " visible" : ""}`} />
+      <dialog
+        ref={dialogRef}
+        onCancel={onClose}
+        className={`dialog ${open ? "open" : "closed"}${className ? ` ${className}` : ""}`}
+        style={style}
+      >
+        {noClose ? null : (
+          <button className="xButton" onClick={onClose}>
+            <svg
+              aria-hidden="true"
+              width="16"
+              height="16"
+              viewBox="0 0 24 24"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                d="M18 6L6 18M6 6L18 18"
+                stroke="currentColor"
+                strokeWidth="2"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+              />
+            </svg>
+          </button>
+        )}
+        {children}
+      </dialog>
+    </>
   );
 };
 
